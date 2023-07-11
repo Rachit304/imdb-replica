7ba116b898be0a273e87856aff9a3f614ed9abe1
Rachit Desai <Rachit.Desai@RACHITDES-LT>
2023-07-11 12:31:19 +0530

    caraousal-update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './component/header/Header'
-import MovieList from './component/movieList/MovieList';
-import Footer from './component/footer/Footer';
-import ScrollToTopButton from './component/scroll/ScrollToTopButton';
-import MovieDetail from './component/movieDetail/MovieDetail';
-
+import Home from './pages/home/Home'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Header />
         <Routes>
-          <Route index element={<h1>Rachit</h1>} />
-          <Route path="movie/:id" element={<MovieDetail/>} />
-          <Route path="movies/:type" element={<MovieList />} />
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<h1>Movie Detail Page</h1>} />
+          <Route path="movies/:type" element={<h1>Movie List Page</h1>} />
           <Route path='/*' element={<h1>Error </h1>} />
         </Routes>
         
