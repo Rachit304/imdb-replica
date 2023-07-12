8715b3587604d4c23c2180b6b06383a8c99ea27b
bhavana chowdhary <Bhavana.Chowdhary@trivium-esolutions.com>
2023-07-12 12:28:24 +0530

    Scroll Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import Header from './component/header/Header'
 import MovieList from './component/movieList/MovieList';
 import Footer from './component/footer/Footer';
 import ScrollToTopButton from './component/scroll/ScrollToTopButton';
-import MovieDetail from './component/movieDetail/MovieDetail';
+
 
 import Home from './pages/home/Home'
 function App() {
   return (
     <div className="App">
-      <Header />
+      
+      
 
       <BrowserRouter>
-     
+     <Header/>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="movie/:id" element={<MovieDetail/>} />
+          
           <Route path="movies/:type" element={<MovieList/>} />
-          <Route path='/*' element={<h1>Error </h1>} />
+         
         </Routes>
         
       </BrowserRouter>
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Card from "../card/Card";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+
 import MovieDetail from "../movieDetail/MovieDetail";
 
 const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSearch = async () => {
     setIsLoading(true);
@@ -16,6 +18,7 @@ const Navbar = () => {
         `https://api.themoviedb.org/3/search/movie?api_key=4e44d9029b1270a757cddc766a1bcb63&query=${searchTerm}`
       );
       setMovies(response.data.results);
+      navigate("/search"); 
     } catch (error) {
       console.log(error);
     }
@@ -27,34 +30,31 @@ const Navbar = () => {
   };
 
   return (
-    <Router>
+    
       <div>
         <input type="text" value={searchTerm} onChange={handleChange} />
         <button onClick={handleSearch}>Search</button>
 
         <Routes>
-          <Route path="/" element={<Home movies={movies} isLoading={isLoading} />} />
+          
+          <Route path="/search" element={<SearchResults movies={movies} />} />
           <Route path="/movie/:id" element={<MovieDetail />} />
+
         </Routes>
       </div>
-    </Router>
+      
   );
 };
 
-const Home = ({ movies, isLoading }) => {
+
+
+const SearchResults = ({ movies }) => {
   return (
     <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          ))}
-        </div>
-      )}
+      {movies.map((movie) => (
+        <Card key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
-
 export default Navbar;
